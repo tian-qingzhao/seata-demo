3397d4a20957007e0991295b4dda00eafd14e708
tx_tianqingzhao <985391294@qq.com>
2024-03-12 15:26:43 +0800

    完善AT模式

---
+++ seata-at/order-service/src/main/java/com/tqz/seata/order/dto/OrderDTO.java
@@ -0,0 +1,19 @@
+package com.tqz.seata.order.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>订单DTO
+ *
+ * @author tianqingzhao
+ * @since 2021/7/12 17:01
+ */
+@Data
+public class OrderDTO implements Serializable {
+    private Integer id;
+    private String accountCode;
+    private String productCode;
+    private Integer count;
+}
